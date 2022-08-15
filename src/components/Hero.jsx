5268329f5b82c9b96fcb66e72e1de14d219487f2
abrusu-collins <snillochemingson@gmail.com>
2022-08-15 22:28:12 +0000

    Added slick slider

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,3 @@
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
 import img1 from "../img/img1.png"
 import img2 from "../img/img2.png"
 import img3 from "../img/img3.png"
@@ -15,22 +9,7 @@ function Hero() {
         YuTunes + YuPods
       </h1>
 
-      <Swiper
-      modules={[Navigation, Pagination, Scrollbar, A11y]}
-      spaceBetween={50}
-      slidesPerView={2}
-      navigation
-      pagination={{ clickable: true }}
-      scrollbar={{ draggable: true }}
-      onSwiper={(swiper) => console.log(swiper)}
-      onSlideChange={() => console.log('slide change')}
-    >
-      <SwiperSlide><img src={img1} alt="img1" /></SwiperSlide>
-      <SwiperSlide><img src={img3} alt="img3" /></SwiperSlide>
-      <SwiperSlide><img src={img3} alt="img3" /></SwiperSlide>
-      
-      ...
-    </Swiper>
+    
     </div>
   );
 }
