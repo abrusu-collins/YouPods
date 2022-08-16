166eec641f0f76a761ee5818726ea8a42820a11b
abrusu-collins <snillochemingson@gmail.com>
2022-08-16 21:31:04 +0000

    done for the day

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./styles/style.css";
 import Hero from "./components/Hero";
+import Areasongtrends from "./components/Areasongtrends";
 function App() {
   return (
     <>
       <Hero />
+      < Areasongtrends/>
     </>
   );
 }
---
+++ src/components/Areasongtrends.jsx
@@ -0,0 +1,16 @@
+
+function Areasongtrends() {
+    return (
+        <div className="areasongtrends">
+            <h1>Songs Trending in your country</h1>
+
+            <div className="areatrendsongs">
+         
+            </div>
+
+
+        </div>
+      );
+}
+
+export default Areasongtrends;
