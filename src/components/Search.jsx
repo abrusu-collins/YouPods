28e4ee567767ee171275b023c9067b5839854d4e
abrusu-collins <snillochemingson@gmail.com>
2022-08-16 01:32:20 +0000

    smooth

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,12 @@
 function Search() {
-    return (
-        <div className="search">
-            <form action="">
-                <input type="text" name="songorpodcastname" id="songorpodcastname" />
-            </form>
-        </div>
-      );
+  return (
+    <div className="search">
+      <form action="">
+        <input type="text" name="songorpodcastname" id="songorpodcastname" placeholder="Enter song or podcast name" />
+        <a href="ff ">Search</a>
+      </form>
+    </div>
+  );
 }
 
 export default Search;
