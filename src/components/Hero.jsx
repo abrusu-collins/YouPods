02860329f5a6192deef0fc31834a762977113068
abrusu-collins <snillochemingson@gmail.com>
2022-08-16 10:45:40 +0000

    formatting done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,17 +1,13 @@
 import SimpleSlider from "./SimpleSlider";
 import Search from "./Search";
 
-
 function Hero() {
   return (
     <div className="hero">
-      <h1 className="heroh1">
-        YuTunes + YuPods
-      </h1>
+      <h1 className="heroh1">YuTunes + YuPods</h1>
       <p>Listen to your favourite songs and podcasts over here, enjoy </p>
-      <Search/>
-     < SimpleSlider/ >
-    
+      <Search />
+      <SimpleSlider />
     </div>
   );
 }
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,8 +2,15 @@ function Search() {
   return (
     <div className="search">
       <form action="">
-        <input type="text" name="songorpodcastname" id="songorpodcastname" placeholder="Enter song or podcast name" />
-        <a href="ff " className="searchbutton">Search</a>
+        <input
+          type="text"
+          name="songorpodcastname"
+          id="songorpodcastname"
+          placeholder="Enter song or podcast name"
+        />
+        <a href="ff " className="searchbutton">
+          Search
+        </a>
       </form>
     </div>
   );
