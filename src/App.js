5d65cf00a4d40f9f6966f0a4a79d14ff1151d6be
abrusu-collins <snillochemingson@gmail.com>
2022-08-17 11:02:05 +0000

    Pdcast trends done

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./styles/style.css";
 import Hero from "./components/Hero";
 import Areasongtrends from "./components/Areasongtrends";
 import Worldsongtrends from "./components/Worldsongtrends";
+import Trendingpodcasts from "./components/Trendingpodcasts";
 function App() {
   return (
     <>
       <Hero />
       < Areasongtrends/>
       < Worldsongtrends/>
+      <Trendingpodcasts/>
     </>
   );
 }
